38754df22161e4f784c09db31b076070eeb171d0
s3818245 <s3818245@rmit.edu.vn>
2020-04-28 23:27:01 +0700

    add function to update movie title

--- a3/script.js
+++ a3/script.js
@@ -1,20 +1,43 @@
 /* Insert your javascript here */
+
+
+// SECTION FOR FUNCTIONS RELATED TO BOOKING FORM
+
+// Variables
+var movieTitleID = ""
+var seatName = ["seats-STA", "seats-STP", "seats-STC", "seats-FCA", "seats-FCP", "seats-FCC"];
+var seatOGPrice = [19.80, 17.50, 15.30, 30.00, 27.00, 24.00];
+var seatDiscounted = [14.00, 12.50, 11.00, 24.00, 22.50, 21.00];
+var currentQuant = [0, 0, 0, 0, 0, 0]
+var subTotal = 0
+
+// get movie title
+document.querySelectorAll(".synopsis").forEach(item => {
+    item.addEventListener("click",
+        function () {
+            if (movieTitleID != "") {
+                document.getElementById(movieTitleID).setAttribute("hidden", true)
+                newMovieTitleID = this.value
+                document.getElementById(newMovieTitleID).removeAttribute("hidden")
+                movieTitleID = newMovieTitleID
+            }
+            else {
+                newMovieTitleID = this.value
+                document.getElementById(newMovieTitleID).removeAttribute("hidden")
+                movieTitleID = newMovieTitleID
+            }
+        }
+    )
+})
+
+// get quantity user selected
 function getselectedQuany(id) {
     var seatType = document.getElementById(id);
     var selectedQuant = seatType.options[seatType.selectedIndex].value;
     return selectedQuant
 }
 
-
-var seatName = ["seats-STA", "seats-STP", "seats-STC", "seats-FCA", "seats-FCP", "seats-FCC"];
-var seatOGPrice = [19.80, 17.50, 15.30, 30.00, 27.00, 24.00];
-var seatDiscounted = [14.00, 12.50, 11.00, 24.00, 22.50, 21.00];
-var currentQuant = [0, 0, 0, 0, 0, 0]
-
-var subTotal = 0
-//Update total when user select option
-
-
+//Functions to update total
 function findIndex(whichID) {
     var i;
     for (i = 0; i < seatName.length; i++) {
