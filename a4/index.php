9d3204d436754278848ab4106cfda3fd911dae19
s3819293 <s3819293@rmit.edu.vn>
2020-05-11 15:03:34 +0700

    fixed titles

--- a4/index.php
+++ a4/index.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Assignment 3</title>
+  <title>Assignment 4</title>
 
   <!-- Keep wireframe.css for debugging, add your css to style.css -->
   <link id='wireframecss' type="text/css" rel="stylesheet" href="wireframe.css" disabled>
