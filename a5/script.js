d04458e725251b3b683fe05c5d2cd1e19cfc5d30
s3818245 <s3818245@rmit.edu.vn>
2020-05-24 21:20:08 +0700

    Update code for subtotal

--- a5/script.js
+++ a5/script.js
@@ -20,3 +20,42 @@ document.querySelectorAll(".category").forEach(
         })
     }
 )
+
+// Update Quantity + price
+function plus(whichID, price){
+    var whichQty = document.getElementById(whichID + "-qty");
+    var whichSubtotal = document.getElementById(whichID + "-subtotal");
+    whichQty.value = Number(whichQty.value) + 1;
+    var newSubtotal = whichQty*price
+    console.log(whichQty.getAttribute("name") + ' quantity is ' + whichQty.value);
+    console.log(whichSubtotal.getAttribute("name") + ' is:$ ' + newSubtotal);
+    whichSubtotal.innerHTML = "Total: " + newSubtotal.toFixed(2);
+}
+
+function minus(whichID, price){
+    var whichQty = document.getElementById(whichID+"-qty");
+    var whichSubtotal = document.getElementById(whichID+"-subtotal");
+    if (whichQty.value > 0){
+        whichQty.value = Number(whichQty.value) -1;
+    }
+    var newSubtotal = whichQty*price
+    console.log(whichQty.getAttribute("name") + ' quantity is ' + whichQty.value);
+    console.log(whichSubtotal.getAttribute("name") + ' is:$ ' + newSubtotal);
+    whichSubtotal.innerHTML = "Total: " + newSubtotal.toFixed(2);
+}
+
+function updateQuantity(whichID, price) {
+    console.log('check quantity function');
+    var whichQty = document.getElementById(whichID+"-qty");
+    var whichSubtotal = document.getElementById(whichID+"-subtotal");
+    var format = new RegExp("^[0-9]+$");
+    if (!format.test(whichQty.value))
+    {
+        alert('Wrong quantity');
+        return;
+    } 
+    var newSubtotal = whichQty.value * price;
+    console.log(whichQty.getAttribute("name") + ' quantity is ' + whichQty.value);
+    console.log(whichSubtotal.getAttribute("name") + ' is:$ ' + newSubtotal);
+    whichSubtotal.innerHTML = "Total: " + newSubtotal.toFixed(2);
+}
