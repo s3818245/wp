81ef018fd1c66c17d63ca347be2d23cb0afc0c25
s3819293 <s3819293@rmit.edu.vn>
2020-05-26 10:29:57 +0700

    linked nav

--- a5/index.php
+++ a5/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,7 +22,7 @@
 
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    
+
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 
@@ -30,10 +31,10 @@
 </head>
 
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
-<?php
-session_start();
+    <?php
+    session_start();
     $servername = "localhost";
-    $port= 8889;
+    $port = 8889;
     $username = "root";
     $password = "root";
     $dbname = "myDB";
@@ -43,10 +44,10 @@ session_start();
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
-    } 
+    }
 
-// SECTION FOR CREATING DATABASE AND TABLE
-/*    // Create table 
+    // SECTION FOR CREATING DATABASE AND TABLE
+    /*    // Create table 
 $sql = "CREATE TABLE itemData (
     itemName VARCHAR(100),
     itemID VARCHAR(100),
@@ -62,8 +63,8 @@ echo "<p>Error creating table: </p>" . mysqli_error($conn);
 } 
 */
 
-//Failed code
-/*$sql = "INSERT INTO itemData VALUES('Apple Airpods Pro', 'airpods-pro', 'apple in-ear', 'This is', '$100',";
+    //Failed code
+    /*$sql = "INSERT INTO itemData VALUES('Apple Airpods Pro', 'airpods-pro', 'apple in-ear', 'This is', '$100',";
 $sql = "INSERT INTO itemData VALUES('Apple Airpods', 'airpods', 'apple in-ear', 'This is ', '$100', 'airpods.jpg');";
 $sql = "INSERT INTO itemData VALUES('Beats Solo 3', 'beats-solo-3', 'beats on-ear', 'This is ', '$100', 'beats-solo-3.jpg');";
 $sql = "INSERT INTO itemData VALUES('Beats Solo', 'beats-solo', 'beats on-ear', 'This is ', '$100', 'beats-solo.jpg');";
@@ -82,21 +83,21 @@ if (mysqli_query($conn, $sql)) {
     } 
 */
 
-mysqli_close($conn);
-?>
+    mysqli_close($conn);
+    ?>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="navbar-collapse w-100 order-1 order-md-0 dual-collapse2">
             <a class="navbar-brand">Logo</a>
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#Home">Home</a>
+                    <a class="nav-link" href="#Home">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#two">sth2</a>
+                    <a class="nav-link" href="#two">sth2</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#three">sth3</a>
+                    <a class="nav-link" href="#three">sth3</a>
                 </li>
             </ul>
         </div>
@@ -105,37 +106,28 @@ mysqli_close($conn);
         <div class="navbar-collapse w-100 order-1 dual-collapse2">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#wishlist">
-                        <svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                    <a class="nav-link" href="wishlist.php">
+                        <svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                         </svg> Wish list</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#checkout">
-                        <svg class="bi bi-check-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path fill-rule="evenodd"
-                                d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
+                    <a class="nav-link" href="orderform.php">
+                        <svg class="bi bi-check-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
                         </svg> Check Out</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#cart">
-                        <svg class="bi bi-cart3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                    <a class="nav-link" href="cart.php">
+                        <svg class="bi bi-cart3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                         </svg> Cart</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link justified-content-right" href="#login">
-                        <svg class="bi bi-lock" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z" />
+                    <a class="nav-link" href="login.php">
+                        <svg class="bi bi-lock" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z" />
                         </svg> Login</a>
                 </li>
             </ul>
@@ -144,7 +136,7 @@ mysqli_close($conn);
 
 
     <main class="main section">
-        <div class="container-fluid">
+        <div class="container-fluid" id="Home">
             <div class="row">
                 <div class="col-12">
                     <div id="carouselMain" class="carousel slide" data-ride="carousel">
@@ -172,8 +164,8 @@ mysqli_close($conn);
             </div>
             <div class="row justify-content-around">
                 <div class="col-3">CATEGORY
-                <nav class="nav flex-column">
-                        <button class="category nav-link"  value="">All items</button>
+                    <nav class="nav flex-column">
+                        <button class="category nav-link" value="">All items</button>
                         <button class="category nav-link" value="in-ear">in-ear headphones</button>
                         <button class="category nav-link" value="over-ear">Over-ear headphones</button>
                         <button class="category nav-link" value="on-ear">On-ear headphones</button>
@@ -183,51 +175,51 @@ mysqli_close($conn);
                 <div class="col-9">ITEM LIST
                     <div class="row row-cols-md-3 row-cols-1">
                         <?php
-                        
+
                         session_start();
                         $servername = "localhost";
-                        $port= 8889;
+                        $port = 8889;
                         $username = "root";
                         $password = "root";
                         $dbname = "myDB";
-                    
+
                         //create connection 
                         $conn = new mysqli("$servername:$port", $username, $password, $dbname);
                         // Check connection
                         if ($conn->connect_error) {
                             die("Connection failed: " . $conn->connect_error);
-                        } 
+                        }
 
                         $sql = "SELECT * FROM itemData";
                         $result = mysqli_query($conn, $sql);
-                        
+
                         if (mysqli_num_rows($result) > 0) {
                             // output data of each row
-                            while($row = mysqli_fetch_assoc($result)) {
-                                echo '<div class="item '.$row['itemClass'].' col py-2">';
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                echo '<div class="item ' . $row['itemClass'] . ' col py-2">';
                                 echo    '<div class="card" style="max-width:250px, height: 400px;">';
-    
-                                echo    '<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($row['itemImage']) . '" />'; ;
-    
+
+                                echo    '<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($row['itemImage']) . '" />';;
+
                                 echo        '<div class="card-body text-center">';
-                                echo            '<h4 class="card-title">' .$row['itemName'].  '</h4>';
+                                echo            '<h4 class="card-title">' . $row['itemName'] .  '</h4>';
                                 echo            '<p class="card-text"></p>';
                                 echo            '<div style="" class="text-center";>';
-                                echo            'Price:' .$row['itemPrice'];
+                                echo            'Price:' . $row['itemPrice'];
                                 echo            '</div>';
-                                echo            '<form action="infopage-debug.php '.header('Location: infopage-debug.php').'" method="get">';
+                                echo            '<form action="infopage-debug.php ' . header('Location: infopage-debug.php') . '" method="get">';
                                 echo                '<div class="text-center">';
-                                echo                '<input type="hidden" name="info" value="'.$row['itemID'].'">';
+                                echo                '<input type="hidden" name="info" value="' . $row['itemID'] . '">';
                                 echo                '<button type="submit" class="btn btn-secondary pt-1">More information</button>';
                                 echo                '</div>';
                                 echo            '</form>';
                                 echo        '</div>';
                                 echo    '</div>';
-                                echo'</div>';
+                                echo '</div>';
                             }
-                          } else {
+                        } else {
                             echo "0 results";
-                          }
+                        }
                         ?>
                         <!-- CODE FOR CARD 
                         <div class="col">
@@ -251,8 +243,8 @@ mysqli_close($conn);
 
                             </div>
                         </div>
-                        --> 
-                        
+                        -->
+
                     </div>
                     <br>
                     <div>
--- a5/infopage-debug.php
+++ a5/infopage-debug.php
@@ -33,21 +33,21 @@
 <body data-spy="scroll" data-target=".navbar" data-offset="50">
     <?php
     session_start();
-    if (!empty($_GET)){
+    if (!empty($_GET)) {
         $id = $_GET['info'];
         session_start();
         $servername = "localhost";
-        $port= 8889;
+        $port = 8889;
         $username = "root";
         $password = "root";
         $dbname = "myDB";
-                    
+
         //create connection 
         $conn = new mysqli("$servername:$port", $username, $password, $dbname);
         // Check connection
         if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-        }   
+            die("Connection failed: " . $conn->connect_error);
+        }
 
         $sql = "SELECT * FROM itemData WHERE itemID='$id'";
         $result = mysqli_query($conn, $sql);
@@ -58,14 +58,14 @@
             $itemPrice = $row['itemPrice'];
             $itemImage = '<img class="card-img-top" src="data:image/jpg;base64,' . base64_encode($row['itemImage']) . '" />';
             $itemDescription = $row['itemDescription'];
-          } else {
+        } else {
             echo "0 results";
-          }
+        }
         mysqli_close($conn);
     }
-    ?> 
+    ?>
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
         <div class="navbar-collapse w-100 order-1 order-md-0 dual-collapse2">
             <a class="navbar-brand">Logo</a>
             <ul class="navbar-nav mr-auto">
@@ -86,36 +86,27 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link justified-content-right" href="#wishlist">
-                        <svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                        <svg class="bi bi-star" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.523-3.356c.329-.314.158-.888-.283-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767l-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288l1.847-3.658 1.846 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.564.564 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
                         </svg> Wish list</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link justified-content-right" href="#checkout">
-                        <svg class="bi bi-check-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                            <path fill-rule="evenodd"
-                                d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
+                        <svg class="bi bi-check-circle" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path fill-rule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z" />
                         </svg> Check Out</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link justified-content-right" href="#cart">
-                        <svg class="bi bi-cart3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+                        <svg class="bi bi-cart3" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
                         </svg> Cart</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link justified-content-right" href="#login">
-                        <svg class="bi bi-lock" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path fill-rule="evenodd"
-                                d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z" />
+                        <svg class="bi bi-lock" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M11.5 8h-7a1 1 0 0 0-1 1v5a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1zm-7-1a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-7zm0-3a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z" />
                         </svg> Login</a>
                 </li>
             </ul>
@@ -157,7 +148,7 @@
                 <div class="col-md-7 col-12">
                     <div class="col-12">
                         <h1>Product name</h1>
-                        <?php echo $itemName;?>
+                        <?php echo $itemName; ?>
                     </div>
                     <hr>
                     <div class="col-12">
@@ -166,7 +157,7 @@
                     <br>
                     <div class="col-12">
                         price
-                        <?php echo $itemPrice;?>
+                        <?php echo $itemPrice; ?>
                     </div>
                     <br>
                     <div class="col-12">
@@ -174,7 +165,7 @@
                     </div>
                     <br>
                     <div class="col-12">
-                        <?php echo $itemDescription;?>
+                        <?php echo $itemDescription; ?>
 
                         Description: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae eius dolorem
                         officia amet laboriosam nam, error perferendis unde minima dolores et autem libero veritatis
